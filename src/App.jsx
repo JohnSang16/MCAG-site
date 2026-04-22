5d7fe2d48ec834defbde35656f16ead40b8d26d4
John Sang <johnsang1970@gmail.com>
2026-04-22 03:16:19 -0400

    feat(router): register /about, /leaderboard, and /donate routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Leaderboard from './pages/Leaderboard'
+import Donate from './pages/Donate'
 import NotFound from './pages/NotFound'
 
 export default function App() {
@@ -7,6 +10,9 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/donate" element={<Donate />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
