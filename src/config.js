0d028a2c99c6904979615f73486c25550a9eaf14
John Sang <johnsang1970@gmail.com>
2026-04-23 00:21:39 -0400

    chore(config): set Facebook URL to real church profile link

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const FACEBOOK_URL = "TBD";
+export const FACEBOOK_URL = "https://www.facebook.com/profile.php?id=100071702324895";
 export const ZELLE_NUMBER = "404-207-6509";
 export const CONTACT_EMAIL = "johnsang1970@gmail.com";
