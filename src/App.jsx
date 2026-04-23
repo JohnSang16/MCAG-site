5444763e749fee8ba8f123c294e0bc1749f562e2
John Sang <johnsang1970@gmail.com>
2026-04-22 20:13:18 -0400

    feat(router): add /about sub-routes for history, beliefs, and jesus

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import AboutHistory from './pages/AboutHistory'
+import AboutBeliefs from './pages/AboutBeliefs'
+import AboutJesus from './pages/AboutJesus'
 import Leaderboard from './pages/Leaderboard'
 import Donate from './pages/Donate'
 import NotFound from './pages/NotFound'
@@ -11,6 +14,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/about/history" element={<AboutHistory />} />
+        <Route path="/about/beliefs" element={<AboutBeliefs />} />
+        <Route path="/about/jesus" element={<AboutJesus />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/donate" element={<Donate />} />
         <Route path="*" element={<NotFound />} />
